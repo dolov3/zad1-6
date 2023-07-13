6773e3906aa545ca6a409ad666d7dda94616d72c
dolov3 <vmalanich572@gmail.com>
2023-07-13 14:50:54 +0200

    Tworzenie i pobieranie info

--- zad5/Client.php
+++ zad5/Client.php
@@ -47,4 +47,36 @@ class Order
     }
 }
 
+// Tworzenie klienta, pracownika i zamówień
+
+$client = new Client(1, 'Jan');
+$employee = new Employee('Maks');
+$order1 = new Order(1, 'Product A');
+$order2 = new Order(2, 'Product B');
+$order3 = new Order(3, 'Product C');
+
+$client->setEmployee($employee);
+$client->addOrder($order1);
+$client->addOrder($order2);
+$client->addOrder($order3);
+
+// Pobieranie informacji o kliencie, pracowniku i zamówieniach
+
+$clientId = 1; // Id klienta
+
+if ($client->id === $clientId) {
+    $clientName = $client->name;
+    $employeeName = $client->employee->name;
+    $recentOrders = array_slice($client->orders, -5);
+
+    echo "Client Name: $clientName.\n";
+    echo "Assigned Employee: $employeeName.\n";
+    echo "Recent Orders:\n";
+    foreach ($recentOrders as $order) {
+        echo "Order ID: {$order->id}, Item: {$order->item}.\n";
+    }
+} else {
+    echo "Client not found\n";
+}
+
 ?>
