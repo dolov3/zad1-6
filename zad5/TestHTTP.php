15c347d8a8d35c09b8b121a40c8a56b2f86d44a7
dolov3 <vmalanich572@gmail.com>
2023-07-13 15:25:52 +0200

    Zad5.3.

---
+++ zad5/TestHTTP.php
@@ -0,0 +1,39 @@
+<?php 
+class Vehicle
+{
+    public $id;
+    public $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+class Client
+{
+    public $id;
+    public $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+class Employee
+{
+    public $id;
+    public $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+
+?>
